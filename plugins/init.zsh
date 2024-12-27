891cae4f2a9e93c0e1a21b5953484d2a8c51a38a
ingenarel <ingenarelitems@gmail.com>
2024-12-28 01:43:09 +0600

    git clones only main branch depth 1 for zinit

--- plugins/init.zsh
+++ plugins/init.zsh
@@ -1,6 +1,6 @@
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
-[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git --branch main --depth 1 "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 source ~/.config/zsh/plugins/fzf-tab-setup.zsh
 zinit load zsh-users/zsh-autosuggestions
