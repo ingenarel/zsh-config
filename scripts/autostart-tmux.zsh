e1332156ea5c9f4913a3e41d7078ab8219cd26b5
ingenarel <ingenarelitems@gmail.com>
2025-01-01 03:40:47 +0600

    tmux session also starts hyfetch

--- scripts/autostart-tmux.zsh
+++ scripts/autostart-tmux.zsh
@@ -1,3 +1,7 @@
 if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ]; then
-    exec tmux new-session -A -s ${USER} >/dev/null 2>&1
+    tmux has-session -t ${USER} 2>/dev/null
+    if [ $? != 0 ]; then
+        tmux new-session -d -s ${USER} 'hyfetch; exec $SHELL'
+    fi
+    exec tmux attach-session -t ${USER} >/dev/null 2>&1
 fi
