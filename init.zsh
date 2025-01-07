723da6b0e014b0f78fe42bb90fbe4952be5fde78
ingenarel <ingenarelitems@gmail.com>
2025-01-08 05:08:59 +0600

    removed fzf zsh completions sourcing cz shit breaks stuff

--- init.zsh
+++ init.zsh
@@ -21,7 +21,6 @@ alias zf='__zoxide_zi'
 alias btop='sudo btop'
 alias discordo='discordo -token "$(gpg -d ~/.config/wofi/token.gpg)"'
 eval "$(starship init zsh)"
-source <(fzf --zsh)
 eval $(thefuck --alias f)
 alias graph="pacgraph --svg -b #000000 -l #013d35 -t #f40cfc -d #ffffff --show-req-by --by-area --explicits --opt-deps -f ~/Images/pacgraph/pacgraph"
 alias musicback="rclone sync /mnt/G/system\ files\(G\)/music drive-alt:music --progress --drive-use-trash=false --delete-excluded"
