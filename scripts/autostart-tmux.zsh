836a6e6e6835658011c1547328d4fff544d5e500
ingenarel <ingenarelitems@gmail.com>
2025-01-12 02:31:33 +0600

    (change): hyfetch -> fastfetch

--- scripts/autostart-tmux.zsh
+++ scripts/autostart-tmux.zsh
@@ -1,7 +1,7 @@
 if [ -x "$(command -v tmux)" ] && [ -n "${DISPLAY}" ] && [ -z "${TMUX}" ]; then
     tmux has-session -t ${USER} 2>/dev/null
     if [ $? != 0 ]; then
-        tmux new-session -d -s ${USER} 'hyfetch; exec $SHELL'
+        tmux new-session -d -s ${USER} 'fastfetch; exec $SHELL'
     fi
     exec tmux attach-session -t ${USER} >/dev/null 2>&1
 fi
