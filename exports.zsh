c7e80895ddfb6f4238ed35613f0d3820b3e1647c
ingenarel <ingenarelitems@gmail.com>
2025-01-14 04:58:44 +0600

    (change): $CC is clang now

--- exports.zsh
+++ exports.zsh
@@ -2,5 +2,6 @@ export CC=gcc
 export CFLAGS="-Wall -Wextra -Wshadow -Wconversion -Wformat=2 -Winline -Wunsafe-loop-optimizations -Wundef\
     -Wpointer-arith -Wpedantic -Wstrict-aliasing -Wcast-align -march=native\
     -mtune=native -O2"
+export CC=clang
 export EDITOR="/usr/bin/nvim"
 export FZF_DEFAULT_OPTS="--bind=alt-n:down,alt-p:up"
