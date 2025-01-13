fe5d4a18c1f85a8671f25a8e545c194ef39354c7
ingenarel <ingenarelitems@gmail.com>
2025-01-14 04:52:39 +0600

    (change): path is sourced first

--- init.zsh
+++ init.zsh
@@ -14,6 +14,7 @@ bindkey -v
 
 source ~/.config/zsh/plugins/init.zsh
 
+path+=("$HOME/.local/pip-bins" "$HOME/.local/share/nvim/mason/bin" "$HOME/.cargo/bin")
 eval "$(zoxide init --no-aliases zsh)"
 alias z='__zoxide_z'
 alias zf='__zoxide_zi'
@@ -22,7 +23,6 @@ alias discordo='discordo -token "$(gpg -d ~/.config/wofi/token.gpg)"'
 eval "$(starship init zsh)"
 alias graph="pacgraph --svg -b #000000 -l #013d35 -t #f40cfc -d #ffffff --show-req-by --by-area --explicits --opt-deps -f ~/Images/pacgraph/pacgraph"
 alias musicback="rclone sync /mnt/G/system\ files\(G\)/music drive-alt:music --progress --drive-use-trash=false --delete-excluded"
-path+=("$HOME/.local/pip-bins" "$HOME/.local/share/nvim/mason/bin" "$HOME/.cargo/bin")
 export CC=gcc
 export CFLAGS="-Wall -Wextra -Wshadow -Wconversion -Wformat=2 -Winline -Wunsafe-loop-optimizations -Wundef\
     -Wpointer-arith -Wpedantic -Wstrict-aliasing -Wcast-align -march=native\
