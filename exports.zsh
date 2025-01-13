f839bfcf139f1043325b6eecec89f40947c5f88c
ingenarel <ingenarelitems@gmail.com>
2025-01-14 04:59:24 +0600

    (feat): added dynamic $CFLAGS based on $CC

--- exports.zsh
+++ exports.zsh
@@ -1,7 +1,14 @@
-export CC=gcc
-export CFLAGS="-Wall -Wextra -Wshadow -Wconversion -Wformat=2 -Winline -Wunsafe-loop-optimizations -Wundef\
-    -Wpointer-arith -Wpedantic -Wstrict-aliasing -Wcast-align -march=native\
-    -mtune=native -O2"
 export CC=clang
+if [ "$CC" = "clang" ]; then
+    export CFLAGS="-Wall -Wextra -Wshadow -Wconversion -Wformat=2 -Winline -Wundef\
+        -Wpointer-arith -Wpedantic -Wstrict-aliasing -Wcast-align -march=native\
+        -mtune=native -O2"
+elif [ "$CC" = "gcc" ]; then
+    export CFLAGS="-Wall -Wextra -Wshadow -Wconversion -Wformat=2 -Winline -Wunsafe-loop-optimizations -Wundef\
+        -Wpointer-arith -Wpedantic -Wstrict-aliasing -Wcast-align -march=native\
+        -mtune=native -O2"
+else
+    export CFLAGS="-Wall -Wextra -march=native -mtune=native -O2"
+fi
 export EDITOR="/usr/bin/nvim"
 export FZF_DEFAULT_OPTS="--bind=alt-n:down,alt-p:up"
